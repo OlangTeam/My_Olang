4899c8867d3fd5d06abecf9c845df81a577bc94d
YonnieeKim <ivg06049@gmail.com>
2021-09-08 21:47:53 +0900

    shape_line_gray.xml

--- app/src/main/java/com/yony/my_olang/ChatCallView.java
+++ app/src/main/java/com/yony/my_olang/ChatCallView.java
@@ -1,8 +1,8 @@
 package com.yony.my_olang;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
 
 public class ChatCallView extends AppCompatActivity {
 
@@ -10,5 +10,18 @@ public class ChatCallView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_call_view);
+    }
+
+    public void mutemode(View view) {
+
+    }
+
+    public void blindmode(View view) {
+    }
+
+    public void speakermode(View view) {
+    }
+
+    public void callexit(View view) {
     }
 }

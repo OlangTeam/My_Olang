8511bed44fb4f428541ff389d001f8bebe534d50
gxxdjin <whdmswls0528@naver.com>
2021-09-10 13:28:03 +0900

    test

---
+++ app/src/main/java/com/yony/my_olang/IdPw_Find.java
@@ -0,0 +1,14 @@
+package com.yony.my_olang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IdPw_Find extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_id_pw_find);
+    }
+}
---
+++ app/src/main/java/com/yony/my_olang/Same_Reccomend_View.java
@@ -0,0 +1,14 @@
+package com.yony.my_olang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Same_Reccomend_View extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_same_reccomend_view);
+    }
+}

0448ef56db8e19fc2049519aa4ae960d806ab4f3
kkkmin <ong06053@naver.com>
2021-09-14 00:26:18 +0900

    권민정-학교 인증, 알림설정, 암호 잠금, 회원 탈퇴 뷰 (1차)

---
+++ app/src/main/java/com/yony/my_olang/AlramSetView.java
@@ -0,0 +1,14 @@
+package com.yony.my_olang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlramSetView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alram_set_view);
+    }
+}
---
+++ app/src/main/java/com/yony/my_olang/SignOutView.java
@@ -0,0 +1,14 @@
+package com.yony.my_olang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignOutView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_out_view);
+    }
+}
